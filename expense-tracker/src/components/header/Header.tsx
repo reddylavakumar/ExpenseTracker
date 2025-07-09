03b8be24becbe0dc07a5a307081a32edf43009dc
lavakumar <lavapkumar@gmail.com>
2025-07-09 12:00:57 +0530

    Json server setup and tanstack query setup for api''s

--- expense-tracker/src/components/header/Header.tsx
+++ expense-tracker/src/components/header/Header.tsx
@@ -1,11 +1,10 @@
-import { Button } from '../ui/button'
 
 const Header = () => {
     return (
         <div>
-            <div className=' w-100vw bg-gray-300 p-3 text-xl flex justify-between position-fixed'>
-                <header>Expense Tracker</header>
-                <Button className='hover:cursor-pointer' variant="default">Add Expense</Button>
+            <div className='w-100vw bg-white-200 border-b-2 p-3 text-xl flex justify-between position-fixed'>
+                <header className='font-semibold'>Expense Tracker</header>
+
             </div>
         </div>
     )
